101524d0c7b2eb58ee48966675616122a1651e25
lelocle3924 <166092601+lelocle3924@users.noreply.github.com>
2026-01-02 18:01:53 +0700

    data

--- plot_monitor.py
+++ plot_monitor.py
@@ -1,5 +1,5 @@
 from plot_results import plot_learning_curve
 
 if __name__ == "__main__":
-    monitor_csv_path = "models/monitor.csv"
+    monitor_csv_path = "models/model_170_125K/monitor55K.csv"
     plot_learning_curve(monitor_csv_path)
